b81b9694f27848dc312b358ffb5be963d10832c7
Suryansh Yadav <suryanshyadav159@gmail.com>
2024-06-09 22:38:47 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,8 +55,8 @@ const Home = () => {
       >
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className='max-w-[1150px] relative m-auto mobxl:flex flex-col justify-center'>
-          <h3 className="font-extrabold text-[25px] text-white capitalize z-10 px-4 pt-[35%] mobxl:pt-0 mobxl:mt-[100px] mobxl:text-[40px] ">
-            Beginning of something <br /> <span className='text-[#a2e3e3] uppercase'>new</span>  starts with Owning a <br /> <span className='text-[#a2e3e3] uppercase'>house </span>
+          <h3 className="font-extrabold text-[21px] text-white capitalize z-10 px-4 pt-[35%] mobxl:pt-0 mobxl:mt-[100px] mobxl:text-[30px] ">
+          Discover the Start of Something Great:<br /> <span className='text-[#a2e3e3] uppercase'> Your One-Stop Destination for All Your E-commerce Needs! </span>
           </h3>
           <button className='z-10 bg-white mx-4 mt-5 w-[100px] rounded h-10'> Explore</button>
         </div>
